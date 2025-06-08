2ca9f2f0457ec5b757789f52003f38c5f2c26e92
Sili Wen <siliwen@gmail.com>
2025-06-08 00:43:04 -0700

    eco

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,8 +1,8 @@
-export default {
+module.exports = {
   apps: [
     {
       name: "inventory-management",
-      script: "pnpm",
+      script: "npm",
       args: "run dev",
       env: {
         NODE_ENV: "development",
